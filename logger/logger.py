83e794cd7f9072ba224997d176b3757950de6e01
Ryan Smith <ryansmith@Ryans-iMac.attlocal.net>
2019-06-29 14:17:10 -0700

    Printing

--- logger/logger.py
+++ logger/logger.py
@@ -18,3 +18,7 @@ GYRz = IMU.readGYRz()
 MAGx = IMU.readMAGx()
 MAGy = IMU.readMAGy()
 MAGz = IMU.readMAGz()
+
+print ("\nACCx %5.2f\tACCy %5.2f\tACCz %5.2f" % (ACCx, ACCy, ACCz))
+print ("\nGYRx %5.2f\tGYRy %5.2f\tGYRz %5.2f" % (GYRx, GYRy, GYRz))
+print ("\nMAGx %5.2f\tMAGy %5.2f\tMAGz %5.2f" % (MAGx, MAGy, MAGz))
