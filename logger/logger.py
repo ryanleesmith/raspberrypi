a1d6ddf1ff244fd104fb353ce3a4d1fa55ca0379
Ryan Smith <ryansmith@Ryans-iMac.attlocal.net>
2019-06-29 14:20:57 -0700

    Loop

--- logger/logger.py
+++ logger/logger.py
@@ -6,19 +6,24 @@ import IMU
 import datetime
 import os
 
+def loop():
+    ACCx = IMU.readACCx()
+    ACCy = IMU.readACCy()
+    ACCz = IMU.readACCz()
+    GYRx = IMU.readGYRx()
+    GYRy = IMU.readGYRy()
+    GYRz = IMU.readGYRz()
+    MAGx = IMU.readMAGx()
+    MAGy = IMU.readMAGy()
+    MAGz = IMU.readMAGz()
+
+    print ("\nACCx %5.2f\tACCy %5.2f\tACCz %5.2f" % (ACCx, ACCy, ACCz))
+    print ("\nGYRx %5.2f\tGYRy %5.2f\tGYRz %5.2f" % (GYRx, GYRy, GYRz))
+    print ("\nMAGx %5.2f\tMAGy %5.2f\tMAGz %5.2f" % (MAGx, MAGy, MAGz))
+
 IMU.detect()
 IMU.init()
 
-ACCx = IMU.readACCx()
-ACCy = IMU.readACCy()
-ACCz = IMU.readACCz()
-GYRx = IMU.readGYRx()
-GYRy = IMU.readGYRy()
-GYRz = IMU.readGYRz()
-MAGx = IMU.readMAGx()
-MAGy = IMU.readMAGy()
-MAGz = IMU.readMAGz()
-
-print ("\nACCx %5.2f\tACCy %5.2f\tACCz %5.2f" % (ACCx, ACCy, ACCz))
-print ("\nGYRx %5.2f\tGYRy %5.2f\tGYRz %5.2f" % (GYRx, GYRy, GYRz))
-print ("\nMAGx %5.2f\tMAGy %5.2f\tMAGz %5.2f" % (MAGx, MAGy, MAGz))
+while True:
+    loop()
+    time.sleep(1)
