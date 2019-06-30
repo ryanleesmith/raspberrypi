8e01f1119ec526684d569ddcbbe46b1dc3cd4e4d
Ryan Smith <ryansmith@Ryans-iMac.attlocal.net>
2019-06-30 00:55:40 -0700

    Remove unused

--- logger/logger.py
+++ logger/logger.py
@@ -4,29 +4,12 @@ import sys
 from os import path, system
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-import time
-import math
 from imu import IMU
-import datetime
-#import os
 
 def loop():
     system('clear')
     IMU.printData()
     return
-    #ACCx = IMU.readACCx()
-    #ACCy = IMU.readACCy()
-    #ACCz = IMU.readACCz()
-    #GYRx = IMU.readGYRx()
-    #GYRy = IMU.readGYRy()
-    #GYRz = IMU.readGYRz()
-    #MAGx = IMU.readMAGx()
-    #MAGy = IMU.readMAGy()
-    #MAGz = IMU.readMAGz()
-
-    #print ("\nACCx %5.2f\tACCy %5.2f\tACCz %5.2f" % (ACCx, ACCy, ACCz))
-    #print ("\nGYRx %5.2f\tGYRy %5.2f\tGYRz %5.2f" % (GYRx, GYRy, GYRz))
-    #print ("\nMAGx %5.2f\tMAGy %5.2f\tMAGz %5.2f" % (MAGx, MAGy, MAGz))
 
 if IMU.detect():
     IMU.initialize()
