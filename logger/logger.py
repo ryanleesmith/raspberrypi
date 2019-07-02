1c6fb74132ec4b078efd0f52efa8ccb84196d2a2
Ryan <ryanlsmith@me.com>
2019-07-01 20:58:58 -0700

    Optimize temp read

--- logger/logger.py
+++ logger/logger.py
@@ -21,7 +21,7 @@ def main():
 
             while True:
                 loop()
-                time.sleep(1)
+                time.sleep(0.25)
     except KeyboardInterrupt:
         print("\nExiting...")
         try:
--- logger/sensors/sensor.py
+++ logger/sensors/sensor.py
@@ -1,3 +1,5 @@
+import time
+
 class Sensor():
     def __init__(self, bus, address, id, name):
         self.idRegister = 0x0F
@@ -48,6 +50,7 @@ class Pressure(Sensor):
     def __init__(self, bus, name):
         Sensor.__init__(self, bus, 0x77, 0x58, name)
         self.idRegister = 0xD0
+        self.lastRead = 0
 
     def initialize(self):
         self.readTrim()
@@ -67,11 +70,63 @@ class Pressure(Sensor):
         if self.trim["T3"] > 32767:
             self.trim["T3"] -= 65536
 
+        self.trim["P1"] = block[7] * 256 + block[6]
+        self.trim["P2"] = block[9] * 256 + block[8]
+        if self.trim["P2"] > 32767:
+            self.trim["P2"] -= 65536
+        self.trim["P3"] = block[11] * 256 + block[10]
+        if self.trim["P3"] > 32767:
+            self.trim["P3"] -= 65536
+        self.trim["P4"] = block[13] * 256 + block[12]
+        if self.trim["P4"] > 32767:
+            self.trim["P4"] -= 65536
+        self.trim["P5"] = block[15] * 256 + block[14]
+        if self.trim["P5"] > 32767:
+            self.trim["P5"] -= 65536
+        self.trim["P6"] = block[17] * 256 + block[16]
+        if self.trim["P6"] > 32767:
+            self.trim["P6"] -= 65536
+        self.trim["P7"] = block[19] * 256 + block[18]
+        if self.trim["P7"] > 32767:
+            self.trim["P7"] -= 65536
+        self.trim["P8"] = block[21] * 256 + block[20]
+        if self.trim["P8"] > 32767:
+            self.trim["P8"] -= 65536
+        self.trim["P9"] = block[23] * 256 + block[22]
+        if self.trim["P9"] > 32767:
+            self.trim["P9"] -= 65536
+
+    def readData(self):
+        currTime = time()
+        if self.lastRead + 1000 < currTime:
+            print "Reading...\n"
+            self.data = self.readBlock(0xF7, 8)
+
+        self.lastRead = currTime
+
 class Thermostat(Pressure):
     def __init__(self, bus):
         Pressure.__init__(self, bus, "Thermostat")
 
     def readTemp(self):
+        self.readData()
+
+        adc_t = ((self.data[3] * 65536) + (self.data[4] * 256) + (self.data[5] & 0xF0)) / 16
+
+        var1 = ((adc_t) / 16384.0 - (self.trim["T1"]) / 1024.0) * (self.trim["T2"])
+        var2 = (((adc_t) / 131072.0 - (self.trim["T1"]) / 8192.0) * ((adc_t)/131072.0 - (self.trim["T1"])/8192.0)) * (self.trim["T3"])
+        #t_fine = (var1 + var2)
+        cTemp = (var1 + var2) / 5120.0
+        return cTemp * 1.8 + 32
+
+    def __str__(self):
+        return "Temperature: %.2f F\n" % self.readTemp()
+
+class Barometer(Pressure):
+    def __init__(self, bus):
+        Pressure.__init__(self, bus, "Barometer")
+
+    def readPressure(self):
         data = self.readBlock(0xF7, 8)
 
         adc_t = ((data[3] * 65536) + (data[4] * 256) + (data[5] & 0xF0)) / 16
