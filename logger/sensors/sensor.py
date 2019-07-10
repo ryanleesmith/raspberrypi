03d09c3042ee22cbc8885c0be5d39c7e409dbbff
Ryan <ryanlsmith@me.com>
2019-07-10 00:04:30 -0700

    F

--- logger/sensors/sensor.py
+++ logger/sensors/sensor.py
@@ -2,7 +2,7 @@ from time import time
 
 def convert(bits, isUnsigned):
     combined = bits[0] | bits[1] << 8
-    return combined if isUnsigned else combined & 0xFFFF
+    return combined if isUnsigned else combined & 0xFFFFF
 
 class Sensor():
     def __init__(self, bus, address, id, name):
