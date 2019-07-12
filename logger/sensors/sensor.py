f163f612a889c8c9ff600cc7115196949111014b
Ryan <ryanlsmith@me.com>
2019-07-11 17:18:17 -0700

    Convert to int

--- logger/sensors/sensor.py
+++ logger/sensors/sensor.py
@@ -240,7 +240,7 @@ class IMU():
 
     def getDirection(self):
         heading = self.getHeading(True)
-        return IMU.DIRECTIONS[round(heading / 22.5)]
+        return IMU.DIRECTIONS[int(round(heading / 22.5))]
 
     def __str__(self):
         output = str(self.acc) + "\n" + str(self.gyr) + "\n" + str(self.mag) + "\n"
